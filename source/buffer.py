656763db4350bf835428cadec04deffa68978ae7
Merlin <merlin@nbi.dk>
2016-11-22 14:59:39 +0100

    Buffer update

--- source/buffer.py
+++ source/buffer.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+
+
 class Buffer():
-    def __init__(self, length, cols = ['data'], filename=''):
+    def __init__(self, length, cols=['data'], filename=''):
         self.filename = filename
         self.length = length
         self.cols = cols
@@ -18,34 +20,41 @@ class Buffer():
         self.size += 1
 
         if self.size >= self.length:
+            self.save_data()
             print('buffer full')
+            step = max(int(self.length / 10), min(self.length, 10))
+            print(step)
+            np.roll(self._data, step)
+            self.size -= step
+            self.data_store_index -= step
             # self.size += x.shape[0]
             # if self.size >= self.length:
             #     self.size = self.length
-                # self.__class__  = RingBufferFull2
+            # self.__class__  = RingBufferFull2
+
     def clear(self):
         self.__init__(self.length, self.cols, self.filename)
 
     def get(self):
         return self._data[:self.size]
 
-
     def save_data(self):
         size = self.size
-        if (self.filename != '') and (self.data_store_index<size):
+        if (self.filename != '') and (self.data_store_index < size):
             # self.saving = True
             # print( 'saving store: ' + self.filename )
             # print( self.data_store_index, self.cols )
             # try:
-            data = self.get()[self.data_store_index:]#.reshape((-1,self._num_cols))
-            df = pd.DataFrame(data, columns = self.cols)
+            # .reshape((-1,self._num_cols))
+            data = self.get()[self.data_store_index:]
+            df = pd.DataFrame(data, columns=self.cols)
 
             # print(df.head())
             # print(df.tail())
             # print(self.size, self.data_store_index)
 
             self.data_store = pd.HDFStore(self.filename)
-            self.data_store.append('data',df,append=True, ignore_index=True)
+            self.data_store.append('data', df, append=True, ignore_index=True)
             self.data_store.close()
             # self.data_store_index = 0
             self.data_store_index = self.size
